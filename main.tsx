2a6db7ab0ddc0a61c5d401ec379ccaa723ae4dcb
Victor Glicério <66135800+ImGlic@users.noreply.github.com>
2024-09-25 19:03:06 -0300

    eu não aguento maisss

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./src/components/header";
 import About from "./src/components/about";
 import Banner from "./src/components/banner";
@@ -13,14 +13,16 @@ import "./src/index.css";
 const App: React.FC = () => {
   return (
     <div className="bg-slate-900 bg-no-repeat bg-cover overflow-hidden">
-      <Header />
-      <Banner />
-      <About />
-      <Projects />
-      <Experience />
-      <Contato />
-      <Footer />
-    </div>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Banner />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/contact" element={<Contato />} />
+    </Routes>
+    <Footer />
+  </div>
   );
 };
 
